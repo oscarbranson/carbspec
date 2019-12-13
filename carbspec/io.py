bc962b11e1f59a15883bf8bc5998a9b87ce78ea2
Oscar Branson <oscarbranson@gmail.com>
2019-12-13 09:23:00 +0000

    load multiple spectra

--- carbspec/io.py
+++ carbspec/io.py
@@ -1,4 +1,6 @@
+import os
 import numpy as np
+from glob import glob
 
 def load_spectrum(file, low=400, high=700, colname_row=0, colname_sep='\t'):
     with open(file, 'r') as f:
@@ -9,3 +11,12 @@ def load_spectrum(file, low=400, high=700, colname_row=0, colname_sep='\t'):
     
     return {k: v for k, v in zip(cols, dat[:, (dat[0] >= low) & (dat[0] <= high)])}
 
+def load_spectra(folder, extension='.dat'):
+    fs = glob(f'{folder}*{extension}')
+
+    spec = {}
+    for f in fs:
+        fn = os.path.splitext(os.path.split(f)[-1])[0]
+        spec[fn] = load_spectrum(f)
+    
+    return spec
