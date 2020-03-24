91aa392b48fd92cc1afbf7142935e10b8dd7eb2e
Oscar <oscarbranson@gmail.com>
2020-03-24 16:23:40 +0000

    remove irrelevant species

--- carbspec/alkalinity/TA.py
+++ carbspec/alkalinity/TA.py
@@ -1,49 +1,6 @@
 import numpy as np
 import scipy.optimize as opt
-from .species import calc_KF, calc_TF, calc_KS, calc_TS, calc_TB, calc_KB
-from carbspec.dye.Ks import calc_KBPB
-
-def TA_from_pH_full(pH, m_sample, m_acid, sal, temp, C_acid, C_dye=0):
-    """
-    Calculate alkalinity from titration end-point pH.
-
-    Equation 6 of Nand & Ellwood (2018, doi:10.1002/lom3.10253)
-
-    Parameters
-    ----------
-    pH : array_like
-        End-point pH of acid addition to seawater.
-    m_sample : array_like
-        Mass of sample.
-    m_acid : array_like
-        Mass of acid added.
-    sal : array_like
-        Salinity of sample.
-    temp : array_like
-        Temperature of sample.
-    C_acid : array_like
-        Concentration of acid
-    
-    Returns
-    -------
-    array_like : Alkalinity in mol kg-1
-    """
-    H = 10**-pH
-
-    TS = calc_TS(sal)
-    TF = calc_TF(sal)
-    TB = calc_TB(sal)
-
-    KS = calc_KS(temp, sal)
-    KF = calc_KF(temp, sal)
-    KB = calc_KB(temp, sal)
-
-    Hfree = H / (1 + TS / KS)
-    HSO4 = TS / (1 + KS / Hfree)
-    HF = TF / (1 + KF / H)
-    BOH4 = TB * KB / (KB + H)
-    
-    return (m_acid * C_acid - (m_sample + m_acid) * (Hfree + HSO4 + HF - BOH4)) / m_sample
+from .species import calc_KF, calc_TF, calc_KS, calc_TS
 
 def TA_from_pH(pH, m_sample, m_acid, sal, temp, C_acid, C_dye=0):
     """
