6b7ece7ab6cc06423b58b243658adba601d22046
Oscar Branson <ob266@cam.ac.uk>
2023-08-10 17:21:02 +0100

    acid strength drift calculation

--- carbspec/alkalinity/acidcal.py
+++ carbspec/alkalinity/acidcal.py
@@ -11,3 +11,13 @@ def acid_zero(acid_str, crm_alk, pH, m0, m, sal, temp):
 def calc_acid_strength(crm_alk, pH, m0, m, sal, temp):
     acid_fit = opt.minimize(acid_zero, 0.1, args=(crm_alk, pH, m0, m, sal, temp), method='Nelder-Mead')
     return acid_fit.x
+
+def acid_drift(p, x, crm_alk, pH, m0, m, sal, temp):
+    acid_str = np.polyval(p, x)
+    TAs = TA_from_pH(pH, m0, m, sal, temp, acid_str) * 1e6
+    return np.sum((unp.nominal_values(TAs - crm_alk)**2))
+
+def calc_acid_strength_drift(x, crm_alk, pH, m0, m, sal, temp, order=1):
+    p0 = [0] * (order - 1) + [0.1]
+    acid_fit = opt.minimize(acid_zero, p0, args=(x, crm_alk, pH, m0, m, sal, temp), method='Nelder-Mead')
+    return acid_fit.x
