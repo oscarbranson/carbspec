02a9288a8cad699a65b771facc5092c732f52048
Oscar Branson <ob266@cam.ac.uk>
2023-08-10 17:21:34 +0100

    functions for calculating acid strength

--- carbspec/alkalinity/TA.py
+++ carbspec/alkalinity/TA.py
@@ -41,10 +41,18 @@ def TA_from_pH(pH, m_sample, m_acid, sal, temp, C_acid, C_dye=0):
     
     return (m_acid * C_acid - (m_sample + m_acid) * (Hfree + HSO4 + HF)) / m_sample
 
-
+# calculate end pH for a given TA
 def TA_diff(pH, TA, m_sample, m_acid, sal, temp, C_acid):
     return (TA_from_pH(pH, m_sample, m_acid, sal, temp, C_acid) - TA)**2
 
 def pH_from_TA(TA, m_sample, m_acid, sal, temp, C_acid):
     pH = opt.newton(TA_diff, 3., args=(TA, m_sample, m_acid, sal, temp, C_acid))
     return pH
+
+# calculate m_acid to reach specified end pH
+def m_acid_diff(m_acid, pH, TA, m_sample, sal, temp, C_acid):
+    return (TA_from_pH(pH=pH, m_sample=m_sample, m_acid=m_acid, sal=sal, temp=temp, C_acid=C_acid) - TA)**2
+
+def calc_m_acid(pH, TA, m_sample, sal, temp, C_acid):
+    m_acid = opt.newton(m_acid_diff, 1., args=(pH, TA, m_sample, sal, temp, C_acid))
+    return m_acid
