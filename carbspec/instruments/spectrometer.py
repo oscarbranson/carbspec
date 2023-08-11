5dcc7205d51a85dadd274491b6482f5388be6f34
Oscar Branson <ob266@cam.ac.uk>
2023-08-11 18:55:10 +0100

    connect to first available by default and report connection

--- carbspec/instruments/spectrometer.py
+++ carbspec/instruments/spectrometer.py
@@ -6,22 +6,26 @@ from seabreeze.spectrometers import list_devices
 
 def list_spectrometers():
     devices = list_devices()
-    print(devices)
     return [f'{s.model} : SN-{s.serial_number}' for s in devices]
 class Spectrometer(sbSpectrometer):
-    def __init__(self, device):
+    def __init__(self, device=None):
+        if device is None:
+            device = list_devices()[0]
+        
         super().__init__(device=device)
                 
         # state
-        self.channel = 0
-        self.light = True
-        self.sample = True
+        # self.channel = 0
+        # self.light = True
+        # self.sample = True
 
         # settings
         self.wvMin = -np.inf
         self.wvMax = np.inf
         
         self.update_wv()
+        
+        print(f'Connected to {self.model} Spectrometer (S/N {self.serial_number}) using `seabreeze`')
             
     @classmethod
     def from_serial_number(cls, serial: str | None = None):
@@ -56,23 +60,23 @@ class Spectrometer(sbSpectrometer):
     def set_integration_time_ms(self, integration_time_ms: int):
         self.integration_time_micros(integration_time_ms * 1e3)
 
-    def channel_0(self):
-        self.channel = 0
+    # def channel_0(self):
+    #     self.channel = 0
 
-    def channel_1(self):
-        self.channel = 1
+    # def channel_1(self):
+    #     self.channel = 1
 
-    def light_on(self):
-        self.light = True
+    # def light_on(self):
+    #     self.light = True
 
-    def light_off(self):
-        self.light = False
+    # def light_off(self):
+    #     self.light = False
 
-    def sample_present(self):
-        self.sample = True
+    # def sample_present(self):
+    #     self.sample = True
 
-    def sample_absent(self):
-        self.sample = False
+    # def sample_absent(self):
+    #     self.sample = False
 
     # def newSample(self):
     #     a = np.random.uniform(.3, .4)
