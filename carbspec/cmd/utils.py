080b0f66349f5d96317661f514522bfa274e9955
Oscar Branson <ob266@cam.ac.uk>
2023-08-25 18:54:18 +0800

    cleanup

--- carbspec/cmd/utils.py
+++ carbspec/cmd/utils.py
@@ -15,7 +15,9 @@ def get_TA_weight_template(dir, overwrite=False):
     """
     template_path = pkgrs.resource_filename('carbspec', 'cmd/resources/TA_weights.ods')
 
-    if os.path.exists(os.path.join(dir, 'TA_weights.ods')) and not overwrite:
+    savepath = os.path.join(dir, 'TA_weights.ods')
+    
+    if os.path.exists(savepath) and not overwrite:
         raise ValueError('TA_weights.ods already exists in this directory. Set overwrite=True to overwrite.')
     
     shutil.copy2(template_path, dir)
